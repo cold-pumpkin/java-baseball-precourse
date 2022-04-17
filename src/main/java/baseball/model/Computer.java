d6e156bd9618ee114b79e2ff609f4d0d2c3e79af
Philip <pch4623@gmail.com>
2022-04-17 11:50:51 +0900

    feat(Computer): answer numbers generation feature
    Added random answer numbers generation method:
    - generate random number and add it if it doesn't exist in the list

--- src/main/java/baseball/model/Computer.java
+++ src/main/java/baseball/model/Computer.java
@@ -1,7 +1,26 @@
 package baseball.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
 public class Computer {
+
+    private static final int NUMBER_OF_DIGITS = 3;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 9;
+    private List<Integer> answerNumbers;
+
     // TODO: 정답 숫자 생성 기능 구현
+    public void generateAnswerNumbers() {
+        answerNumbers = new ArrayList<>();
+        while (answerNumbers.size() < NUMBER_OF_DIGITS) {
+            int number = pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
+            if (!answerNumbers.contains(number))
+                answerNumbers.add(number);
+        }
+    }
 
     // TODO: 결과 판단(볼 카운트) 기능 구현
 
