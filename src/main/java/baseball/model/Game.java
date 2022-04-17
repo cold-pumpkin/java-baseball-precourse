7978e44daa865acfbd27a368377dfdbe60fee885
Philip <pch4623@gmail.com>
2022-04-18 00:51:48 +0900

    feat(Game): add a method to validate player's input

--- src/main/java/baseball/model/Game.java
+++ src/main/java/baseball/model/Game.java
@@ -1,11 +1,18 @@
 package baseball.model;
 
+import static baseball.model.Validator.*;
+
 public class Game {
-    // TODO: 게임 프로세스 구현
 
     // TODO: 숫자 입력 안내 기능 구현
 
     // TODO: 입력값 체크 기능 구현
+    private void validatePlayerInput(String playerInput) {
+        validateInputType(playerInput);
+        validateInputSize(playerInput);
+        validateInputDuplicate(playerInput);
+        validateInputRange(playerInput);
+    }
 
     // TODO: 결과 메시지 출력 기능 구현
 }
