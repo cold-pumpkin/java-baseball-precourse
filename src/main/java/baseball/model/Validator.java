d4e1d39f63c54e72e544b494584097ac3e55b6ce
Philip <pch4623@gmail.com>
2022-04-17 23:56:10 +0900

    feat(Validator): check for duplication in input value

--- src/main/java/baseball/model/Validator.java
+++ src/main/java/baseball/model/Validator.java
@@ -1,5 +1,8 @@
 package baseball.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static baseball.model.Computer.NUMBER_OF_DIGITS;
 
 public class Validator {
@@ -18,6 +21,16 @@ public class Validator {
     public static void validateInputSize(String input) {
         if (input.length() != NUMBER_OF_DIGITS) {
             throw new IllegalArgumentException("숫자 " + NUMBER_OF_DIGITS + "개를 입력해주세요!");
+        }
+    }
+
+    public static void validateInputDuplicate(String input) {
+        Map<Character, Boolean> inputMap = new HashMap<>();
+        for (Character ch : input.toCharArray()) {
+            inputMap.put(ch, true);
+        }
+        if (inputMap.size() != NUMBER_OF_DIGITS) {
+            throw new IllegalArgumentException("숫자는 중복일 수 없습니다.");
         }
     }
 
