6789ce3cad02bebaf7c63bf15c8082e58ecebb8f
Philip <pch4623@gmail.com>
2022-04-18 21:59:31 +0900

    feat: add GameController to init and play game

---
+++ src/main/java/baseball/controller/GameController.java
@@ -0,0 +1,25 @@
+package baseball.controller;
+
+import baseball.model.Game;
+
+import static baseball.model.InputReceiver.getRestartYn;
+import static baseball.view.ControllerMessageViewer.showRestartGuideMessage;
+
+public class GameController {
+
+    public GameController() {}
+
+    public void execute() {
+        Game game = new Game();
+        do {
+            game.init();
+            game.play();
+            showRestartGuideMessage();
+        } while (isRestart());
+    }
+
+    private boolean isRestart() {
+        return "1".equals(getRestartYn());
+    }
+
+}
