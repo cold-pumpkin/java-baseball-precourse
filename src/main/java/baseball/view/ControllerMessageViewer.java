d8ade1818c3ee7f9f434317cccad69dcf67690ac
Philip <pch4623@gmail.com>
2022-04-18 19:32:31 +0900

    feat: add message viewer for use in the game controller

---
+++ src/main/java/baseball/view/ControllerMessageViewer.java
@@ -0,0 +1,11 @@
+package baseball.view;
+
+public class ControllerMessageViewer {
+
+    private static final String RESTART_GUIDE_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    public void showRestartGuideMessage() {
+        System.out.println(RESTART_GUIDE_MESSAGE);
+    }
+
+}
