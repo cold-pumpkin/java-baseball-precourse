0d64af9281d15de12e40ee4a7a8e882e174c982f
Philip <pch4623@gmail.com>
2022-04-18 21:54:39 +0900

    refactor: change access modifier private to public

--- src/main/java/baseball/view/ControllerMessageViewer.java
+++ src/main/java/baseball/view/ControllerMessageViewer.java
@@ -4,7 +4,7 @@ public class ControllerMessageViewer {
 
     private static final String RESTART_GUIDE_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
-    public void showRestartGuideMessage() {
+    public static void showRestartGuideMessage() {
         System.out.println(RESTART_GUIDE_MESSAGE);
     }
 
--- src/main/java/baseball/view/GameMessageViewer.java
+++ src/main/java/baseball/view/GameMessageViewer.java
@@ -5,15 +5,15 @@ public class GameMessageViewer {
     private static final String INPUT_GUIDE_MESSAGE = "숫자를 입력해주세요 : ";
     private static final String ANSWER_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
-    public void showInputGuideMessage() {
+    public static void showInputGuideMessage() {
         System.out.print(INPUT_GUIDE_MESSAGE);
     }
 
-    public void showSuccessMessage() {
+    public static void showSuccessMessage() {
         System.out.println(ANSWER_MESSAGE);
     }
 
-    public void showHintMessage(int balls, int strikes) {
+    public static void showHintMessage(int balls, int strikes) {
         if (balls == 0 && strikes == 0) {
             System.out.println("낫싱");
             return;
