c5e0953dc2081885b995ed46d3e322c52cdd3f9c
Philip <pch4623@gmail.com>
2022-04-18 22:28:45 +0900

    refator: change the position of a code

--- src/main/java/baseball/model/Computer.java
+++ src/main/java/baseball/model/Computer.java
@@ -3,17 +3,16 @@ package baseball.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import static baseball.model.constant.Constraint.*;
 import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
 public class Computer {
 
-    public static final int NUMBER_OF_DIGITS = 3;
-    public static final int MIN_NUMBER = 1;
-    public static final int MAX_NUMBER = 9;
     private List<Integer> answerNumbers;
 
     Computer(){};
 
+    // 테스트 코드에서만 사용
     Computer(List<Integer> answerNumbers) {
         this.answerNumbers = answerNumbers;
     }
@@ -23,7 +22,23 @@ public class Computer {
         generateAnswerNumbers();
     }
 
-    // TODO: 정답 숫자 생성 기능 구현
+    public int countBalls(List<Integer> playerNumbers) {
+        int balls = 0;
+        for (int i = 0; i < NUMBER_OF_DIGITS; i++) {
+            balls += addBallByCondition(answerNumbers.get(i), playerNumbers.get(i));
+        }
+        return balls;
+    }
+
+    public int countStrikes(List<Integer> playerNumbers) {
+        int strikes = 0;
+        for (int i = 0; i < NUMBER_OF_DIGITS; i++) {
+            strikes += addStrikeByCondition(answerNumbers.get(i), playerNumbers.get(i));
+        }
+        return strikes;
+    }
+
+
     private void generateAnswerNumbers() {
         while (answerNumbers.size() < NUMBER_OF_DIGITS) {
             int number = pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
@@ -36,30 +51,12 @@ public class Computer {
             answerNumbers.add(number);
     }
 
-    // TODO: 결과 판단(볼 카운트) 기능 구현
-    public int countBalls(List<Integer> playerNumbers) {
-        int balls = 0;
-        for (int i = 0; i < NUMBER_OF_DIGITS; i++) {
-            balls += addBallByCondition(answerNumbers.get(i), playerNumbers.get(i));
-        }
-        return balls;
-    }
-
     private int addBallByCondition(int answerNumber, int playerNumber) {
         if (answerNumber != playerNumber
                 && answerNumbers.contains(playerNumber)) {
             return 1;
         }
         return 0;
-    }
-
-    // TODO: 결과 판단(스트라이크 카운트) 기능 구현
-    public int countStrikes(List<Integer> playerNumbers) {
-        int strikes = 0;
-        for (int i = 0; i < NUMBER_OF_DIGITS; i++) {
-            strikes += addStrikeByCondition(answerNumbers.get(i), playerNumbers.get(i));
-        }
-        return strikes;
     }
 
     private int addStrikeByCondition(int answerNumber, int playerNumber) {
@@ -69,7 +66,7 @@ public class Computer {
         return 0;
     }
 
-    // ** Getter
+    // 테스트 코드에서만 사용
     public List<Integer> getAnswerNumbers() {
         return answerNumbers;
     }
--- src/test/java/baseball/model/ComputerTest.java
+++ src/test/java/baseball/model/ComputerTest.java
@@ -1,9 +1,10 @@
 package baseball.model;
 
 import org.junit.jupiter.api.*;
-
 import java.util.*;
 
+import static baseball.model.constant.Constraint.MAX_NUMBER;
+import static baseball.model.constant.Constraint.MIN_NUMBER;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ComputerTest {
@@ -16,26 +17,26 @@ class ComputerTest {
         computer.init();
     }
 
-    @DisplayName("세자리의 정답 숫자가 정상적으로 생성되었는지 확인한다.")
     @Test
+    @DisplayName("세자리의 정답 숫자가 정상적으로 생성되었는지 확인한다.")
     void generateAnswerNumbersTest() {
         assertThat(computer.getAnswerNumbers())
                 .isNotEmpty()
                 .hasOnlyElementsOfType(Integer.class)
                 .hasSize(3)
-                .doesNotContain(0);
+                .allSatisfy(number -> assertThat(number).isBetween(MIN_NUMBER, MAX_NUMBER));
     }
 
-    @DisplayName("정답 숫자에 중복이 없는지 확인한다.")
     @Test
+    @DisplayName("정답 숫자에 중복이 없는지 확인한다.")
     void answerNumberDuplicateTest() {
         Set<Integer> answerSet = new HashSet<>(computer.getAnswerNumbers());
         assertThat(answerSet)
                 .hasSize(3);
     }
 
-    @DisplayName("볼 카운트 기능을 확인한다.")
     @Test
+    @DisplayName("볼 카운트 기능을 확인한다.")
     void countBallsTest() {
         List<Integer> playNumbers = Arrays.asList(6, 4, 1);
         List<Integer> answerNumbers = Arrays.asList(1, 4, 6);
@@ -45,8 +46,8 @@ class ComputerTest {
                 .isEqualTo(2);
     }
 
-    @DisplayName("스트라이크 카운트 기능을 확인한다.")
     @Test
+    @DisplayName("스트라이크 카운트 기능을 확인한다.")
     void countStrikesTest() {
         List<Integer> playNumbers = Arrays.asList(6, 4, 1);
         List<Integer> answerNumbers = Arrays.asList(1, 4, 6);
